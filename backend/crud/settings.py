1cef6398124a25dc5ba9d0e24c0a5bd5ad74a4ed
gokhanmeteerturk <me@e-mete.com>
2024-11-20 16:48:42 +0000

    cors fix

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWEED_ORIGINS = [
+     "http://localhost:5173"
+]
+
+
 
 ROOT_URLCONF = 'crud.urls'
 
