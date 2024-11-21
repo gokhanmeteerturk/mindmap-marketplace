353678304c13d4c818e9062b9a04efb5e84ce455
gokhanmeteerturk <me@e-mete.com>
2024-11-21 15:04:35 +0000

    fix backend url

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://127.0.0.1:8000',
-  baseURL: 'https://cloudcomputing-django-gme.uksouth-01.azurewebsites.net/', 
+  baseURL: 'https://cloudcomputing-django-gme.azurewebsites.net', 
   timeout: 5000,
 });
 
